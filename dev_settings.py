8a361f0234bcf98aacc019e47aaebae70d8e1505
Junjun Zhang <junjun.ca@gmail.com>
2015-06-14 17:56:47 -0400

    minor changes to settings files

--- dev_settings.py
+++ dev_settings.py
@@ -4,7 +4,7 @@ DEBUG = True
 
 ES_HOST = os.getenv("ES_HOST", "localhost")
 ES_PORT = int(os.getenv("ES_PORT", "9200"))
-ES_INDEX = "p_150507193855"
+ES_INDEX = os.getenv("ES_INDEX", "p_150507193855")
 
 API_HOST = os.getenv("API_HOST", "localhost")
 API_PORT = int(os.getenv("API_PORT", "5000"))
--- prod_settings.py
+++ prod_settings.py
@@ -9,4 +9,5 @@ ES_INDEX = os.getenv("ES_INDEX", "pcawg_es")
 API_HOST = os.getenv("API_HOST", "0.0.0.0")
 API_PORT = int(os.getenv("API_PORT", "9000"))
 
-SERVER_NAME = "pancancer.info"
+# do not need this, as we run behind apache
+#SERVER_NAME = "pancancer.info"
