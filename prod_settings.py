2bec75ec21da57c7c904053dc8d0cd506ec290ed
Junjun Zhang <junjun.ca@gmail.com>
2015-06-14 16:28:45 -0400

    changed prod port

--- prod_settings.py
+++ prod_settings.py
@@ -7,6 +7,6 @@ ES_PORT = int(os.getenv("ES_PORT", "9200"))
 ES_INDEX = "pcawg_es"
 
 API_HOST = os.getenv("API_HOST", "0.0.0.0")
-API_PORT = int(os.getenv("API_PORT", "5000"))
+API_PORT = int(os.getenv("API_PORT", "9000"))
 
 SERVER_NAME = "pancancer.info"
